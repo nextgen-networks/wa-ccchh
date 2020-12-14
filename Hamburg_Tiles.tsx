929673eb07f51a58bfcac00baabe10e83412ab34
Andreas Sons <sons@Lymann-Alpha.local>
2020-12-15 00:26:40 +0100

    Kuddl hat die Stubnitz gezeichnet, DL8XAS hat sie mal in der Hamburg-Map eingetragen.

---
+++ Hamburg_Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Hamburg_Tiles" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="Hamburg_Tiles.png" width="96" height="64"/>
+</tileset>
