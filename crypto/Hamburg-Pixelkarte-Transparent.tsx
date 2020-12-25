efdc151db8950ba72485b2460195fa48f3fed41e
Andreas Sons <sons@Lymann-Alpha.local>
2020-12-26 00:39:07 +0100

    Cryptoparty hinzugefügt.

---
+++ crypto/Hamburg-Pixelkarte-Transparent.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Hamburg-Pixelkarte-Transparent" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../Hamburg-Pixelkarte-Transparent.png" width="96" height="96"/>
+</tileset>
