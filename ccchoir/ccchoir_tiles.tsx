53f7f7510e9d6f5638436378a1df024189e2d1ba
Andreas Sons <sons@Lymann-Alpha.local>
2020-12-25 22:59:18 +0100

    Kollision in einen eigenen Tileset auslagern.

---
+++ ccchoir/ccchoir_tiles.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="ccchoir_tiles" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="ccchoir_tiles.png" width="320" height="320"/>
+ <tile id="0">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
