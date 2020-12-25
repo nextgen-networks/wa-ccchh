622eb133ae2b2e79179f3fbe928bd2e040f8f614
Andreas Sons <sons@Lymann-Alpha.local>
2020-12-25 22:33:38 +0100

    Eigenschaft "Kollision" war nicht gesetzt.

---
+++ ccchoir/Aumuehle_Tiles.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Aumuehle_Tiles" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../Aumuehle_Tiles.png" width="320" height="320"/>
+ <tile id="4">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
