2e5de0180a6569d9f8b5b1117a25cf43adab473e
Andreas Sons <sons@indusi.net>
2020-12-27 23:27:04 +0100

    Der Chaosknoten für Vollkorns Mitgliederbetreuung hinzu.

---
+++ chaosknoten/Water+.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Water+" tilewidth="32" tileheight="32" tilecount="42" columns="6">
+ <image source="Water+.png" width="192" height="224"/>
+ <tile id="0">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
