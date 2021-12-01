be2524b4968cbb5c2917ce02cd66c5fdab255706
djerun <djerun@hamburg.ccc.de>
2021-12-01 23:05:59 +0100

    add animated tiles for non-rotated versions of new textures

---
+++ djerun-test/pulsing_corner_neon_pink.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pulsing_corner_neon_pink" tilewidth="32" tileheight="32" tilecount="8" columns="8">
+ <image source="animated_tile_neon_pink_corner/frames.png" width="256" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="400"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="400"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
