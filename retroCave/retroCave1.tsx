370fa50ddd9c2dfd0f50ad26900fbae594ca7edc
Hans vom Schloß <internetkontor@vomSchloss.de>
2021-12-04 00:01:08 +0100

    LYNX' retroCave * restart

--- retroCave/retroCave1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="retroCave1" tilewidth="32" tileheight="32" tilecount="1400" columns="50">
- <image source="retroCave1.png" width="1600" height="900"/>
-</tileset>
