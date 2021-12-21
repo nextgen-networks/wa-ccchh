8867e3685f98f65706d79a23f1d354e59f6bac6c
foorbie <me@foorbie.de>
2021-12-21 21:34:16 +0100

    block collides

--- block.tsx
+++ block.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="block" tilewidth="32" tileheight="32" tilecount="1" columns="1">
  <image source="block.png" width="32" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
