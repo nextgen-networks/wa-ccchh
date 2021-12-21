c7187419ef13318a08c3b6431620356c292defdf
foorbie <me@foorbie.de>
2021-12-21 13:59:48 +0100

    Schrift-2

--- schriftzug-hh.tsx
+++ schriftzug-hh.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="schriftzug-hh" tilewidth="32" tileheight="32" tilecount="200" columns="20">
- <image source="../Pictures/schriftzug-ccchh.png" width="640" height="320"/>
+ <image source="cmsnord/schriftzug-ccchh.png" width="640" height="320"/>
 </tileset>
