5c6baf0be30543562a8b59a559a093761804379a
foorbie <me@foorbie.de>
2021-12-21 21:29:48 +0100

    added block-tile

---
+++ block.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="block" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="block.png" width="32" height="32"/>
+</tileset>
