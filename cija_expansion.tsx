85b80f1eef451c15ea1eff554fd58e7f4cd64c76
djerun <djerun@hamburg.ccc.de>
2021-12-24 15:21:47 +0100

    add licence from png to tileset using it

--- cija_expansion.tsx
+++ cija_expansion.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="cija_expansion" tilewidth="32" tileheight="32" tilecount="1995" columns="15">
+ <properties>
+  <property name="tilesetCopyright" value="CC0"/>
+ </properties>
  <image source="cija_32x32_expansion-for-Pipoya_CC0.png" width="480" height="4256"/>
 </tileset>
