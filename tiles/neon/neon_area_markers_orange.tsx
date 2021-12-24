72ea8f220ac2959a66b3d5daf24a84b33089b7e1
djerun <djerun@hamburg.ccc.de>
2021-12-24 21:21:39 +0100

    mark jitsi areas

---
+++ tiles/neon/neon_area_markers_orange.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="neon_area_markers_orange" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <properties>
+  <property name="tilesetCopyright">author: djerun
+licence: CC BY-SA 4.0</property>
+ </properties>
+ <image source="neon_area_markers_orange.png" width="96" height="96"/>
+</tileset>
