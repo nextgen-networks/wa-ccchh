3d7364b915b94c3800215608946d05c9d9c15ee2
djerun <djerun@hamburg.ccc.de>
2021-12-24 13:56:16 +0100

    add tilesetCopyright, use instead of embedding in tmx

--- tiles/rc3-21/vcr_overlay.tsx
+++ tiles/rc3-21/vcr_overlay.tsx
@@ -1,4 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="vcr_overlay" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <properties>
+  <property name="tilesetCopyright">author: djerun
+licence: CC BY-SA 4.0</property>
+ </properties>
  <image source="VCR_Overlay.png" width="32" height="32"/>
 </tileset>
---
+++ zNEON/pulsing_line_90_neon_pink.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pulsing_line_90_neon_pink" tilewidth="32" tileheight="32" tilecount="8" columns="1">
+ <properties>
+  <property name="tilesetCopyright">author: djerun
+licence: CC BY-SA 4.0</property>
+ </properties>
+ <image source="animated_tile_neon_pink_line/frames_90.png" width="32" height="256"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="400"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="400"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
