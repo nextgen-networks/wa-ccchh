10cd5ac8abebfca23fced626a7b822586638936c
djerun <djerun@hamburg.ccc.de>
2021-12-24 15:59:06 +0100

    add licence for worldExit

--- worldExit.tsx
+++ worldExit.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="worldExit" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <properties>
+  <property name="tilesetCopyright" value="CC0"/>
+ </properties>
  <image source="worldExit.png" width="64" height="64"/>
 </tileset>
