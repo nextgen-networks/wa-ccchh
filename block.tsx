53e6ec1fa99d182a9b11ff934d0c9b17201fc259
foorbie <me@foorbie.de>
2021-12-25 19:23:03 +0100

    added Copyright of block.tsx

--- block.tsx
+++ block.tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="block" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <properties>
+  <property name="tilesetCopyright" value="CC0"/>
+ </properties>
  <image source="block.png" width="32" height="32"/>
  <tile id="0">
   <properties>
