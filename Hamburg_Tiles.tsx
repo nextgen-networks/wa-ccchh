89b20f2186432f658fda337db0c6468b049db539
Hans vom Schloß <internetkontor@vomSchloss.de>
2021-12-25 20:52:22 +0100

    * added tilesetCopyright for main.json and Hamburg_Tiles

--- Hamburg_Tiles.tsx
+++ Hamburg_Tiles.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Hamburg_Tiles" tilewidth="32" tileheight="32" tilecount="128" columns="16">
+<tileset version="1.5" tiledversion="1.7.2" name="Hamburg_Tiles" tilewidth="32" tileheight="32" tilecount="128" columns="16">
+ <properties>
+  <property name="tilesetCopyright" value="CCCHH 2020,2021"/>
+ </properties>
  <image source="Hamburg_Tiles.png" width="512" height="256"/>
 </tileset>
