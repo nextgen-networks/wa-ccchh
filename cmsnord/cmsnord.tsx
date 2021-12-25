7cfa5f3b58bd4dfb01f8452c9d52be9a638af004
foorbie <me@foorbie.de>
2021-12-25 19:03:27 +0100

    added copyright

--- cmsnord/cmsnord.tsx
+++ cmsnord/cmsnord.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="ccmsnord" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="zugang-cmsnord.png" width="80" height="80"/>
+<tileset version="1.5" tiledversion="1.7.2" name="ccmsnord" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <properties>
+  <property name="tilesetCopyright" value="CC0"/>
+ </properties>
+ <image source="zugang-cmsnord.png" width="96" height="96"/>
 </tileset>
--- hamburgkarte.tsx
+++ hamburgkarte.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="hamburgkarte" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <properties>
+  <property name="tilesetCopyright" value="CC0"/>
+ </properties>
  <image source="hamburg-quadrat.png" width="1024" height="1024"/>
 </tileset>
