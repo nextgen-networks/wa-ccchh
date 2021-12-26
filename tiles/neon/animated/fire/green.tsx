3bb29984378cbfe82ed6a779527457e0ac3d2d58
djerun <djerun@hamburg.ccc.de>
2021-12-26 18:35:05 +0100

    add green fire animation

---
+++ tiles/neon/animated/fire/green.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="fire_green" tilewidth="32" tileheight="32" tilecount="15" columns="15">
+ <image source="green.png" width="480" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="200"/>
+   <frame tileid="1" duration="200"/>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+   <frame tileid="8" duration="200"/>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="10" duration="200"/>
+   <frame tileid="11" duration="200"/>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="14" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
